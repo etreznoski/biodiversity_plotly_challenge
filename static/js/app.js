ce4ce60eb913be68f9a9c4d408428cb8c657674c
etreznoski <sanchez.elizabethk@gmail.com>
2021-04-08 11:47:47 -0700

    started work on initial bar graph set up

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,28 @@
+
 d3.json('samples.json').then(data => {
-    
-    console.log(data.names);
-})
 
-function init() {
+    console.log(data);
+    console.log(data.samples[0]);
 
-}
 
-function updatePlotly () {
+    function init() {
+      let otuIdStr = data.samples[1].otu_ids.toString();
+      let trace1 = {
+        x: data.samples[1].sample_values,
+        y: `OTU ${otuIdStr},
+        type: "bar",
+        orientation: "h",
+        name: data.samples[1].id,
+        text: data.samples[1].otu_labels
+      };
+      let data1 = [trace1];
+      let layout1 = {
+        title: data.samples[1].id,
+        yaxis: {tickangle: -45,}
 
-}
+      };
+      Plotly.newPlot('bar', data1, layout1);
 
-init();
+    }
+    init();
+});
